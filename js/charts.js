99e7640fad242a53ca3b1375a2cd606f9e93f9e4
kihaller <67028582+kihaller@users.noreply.github.com>
2020-07-29 18:34:36 -0700

    Addes responsive time series chart, added styling

--- js/charts.js
+++ js/charts.js
@@ -1,28 +1,27 @@
 //Line Chart
 Chart.defaults.global.defaultFontColor = "#A7A7A7";
 
-new Chart(document.getElementById("line-chart"), {
+var lineChart = new Chart(document.getElementById("line-chart"), {
   type: "line",
   data: {
-    labels: [
-      "16 - 22",
-      "23 - 29",
-      "30 - 5",
-      "6 - 12",
-      "13 - 19",
-      "20 - 26",
-      "27 - 3",
-      "4 - 10",
-      "11 - 17",
-      "18 - 24",
-      "25 - 31",
-    ],
     datasets: [
       {
-        data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000],
-        label: "",
+        data: [
+          { x: "2020-03-16 13:00:00", y: 0 },
+          { x: "2020-03-23 13:00:00", y: 500 },
+          { x: "2020-03-29 13:00:00", y: 1000 },
+          { x: "2020-04-02 13:00:00", y: 750 },
+          { x: "2020-04-12 13:00:00", y: 1250 },
+          { x: "2020-04-19 13:00:00", y: 1750 },
+          { x: "2020-04-23 13:00:00", y: 1250 },
+          { x: "2020-05-03 13:00:00", y: 1500 },
+          { x: "2020-05-10 13:00:00", y: 1000 },
+          { x: "2020-05-17 13:00:00", y: 1500 },
+          { x: "2020-05-24 13:00:00", y: 2000 },
+        ],
         borderColor: "#7477BF",
         fill: true,
+        fillColor: "#7477BF",
         borderWidth: 1,
         lineTension: 0,
         pointBorderColor: "#7477BF",
@@ -33,8 +32,10 @@ new Chart(document.getElementById("line-chart"), {
   },
   options: {
     maintainAspectRatio: false,
-    legend: { display: true, position: "top", align: "end" },
 
+    legend: {
+      display: false,
+    },
     scales: {
       yAxes: [
         {
@@ -45,8 +46,41 @@ new Chart(document.getElementById("line-chart"), {
           },
         },
       ],
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            min: "2020-04-24 13:00:00",
+            unit: "week",
+          },
+        },
+      ],
     },
   },
+});
+
+document.getElementById("button-hourly").addEventListener("click", (event) => {
+  lineChart.options.scales.xAxes[0].time.unit = "hour";
+  lineChart.options.scales.xAxes[0].time.min = "2020-05-23 13:00:00";
+  lineChart.update();
+});
+
+document.getElementById("button-daily").addEventListener("click", (event) => {
+  lineChart.options.scales.xAxes[0].time.unit = "day";
+  lineChart.options.scales.xAxes[0].time.min = "2020-05-17 13:00:00";
+  lineChart.update();
+});
+
+document.getElementById("button-weekly").addEventListener("click", (event) => {
+  lineChart.options.scales.xAxes[0].time.unit = "week";
+  lineChart.options.scales.xAxes[0].time.min = "2020-04-24 13:00:00";
+  lineChart.update();
+});
+
+document.getElementById("button-monthly").addEventListener("click", (event) => {
+  lineChart.options.scales.xAxes[0].time.unit = "month";
+  lineChart.options.scales.xAxes[0].time.min = "2020-02-24 13:00:00";
+  lineChart.update();
 });
 
 //Bar Chart
@@ -57,6 +91,8 @@ new Chart(document.getElementById("bar-chart"), {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
     datasets: [
       {
+        barPercentage: 0.5,
+
         backgroundColor: [
           "#7477BF",
           "#7477BF",
