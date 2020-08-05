6881eae67af2d96039a84d59e25b81e5b90ab9ee
kihaller <67028582+kihaller@users.noreply.github.com>
2020-08-04 17:16:29 -0700

    Added functionality to the website

--- js/main.js
+++ js/main.js
@@ -23,12 +23,81 @@ for (i = 0; i < close.length; i++) {
 
 /* Mobile Navigation*/
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+const hamburgerNav = document.getElementById("hamburger-link");
+hamburgerNav.addEventListener("click", function () {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+    const popup = document.getElementById("popup-container");
+    popup.style.visibility = "hidden";
   }
-}
-document.getElementById("hamburger-link").addEventListener("click", myFunction);
+});
+
+/* Display notifications when user clicks on bell icon*/
+
+const notification = document.getElementById("notification-icon");
+notification.addEventListener("click", function () {
+  const popup = document.getElementById("popup-container");
+  popup.style.visibility = "visible";
+});
+
+/* Local Storage Settings */
+
+const timezone = document.getElementById("timezone-select");
+const save = document.getElementById("save-button");
+const cancel = document.getElementById("cancel-button");
+const switch1 = document.getElementById("email-settings");
+const switch2 = document.getElementById("profile-settings");
+
+save.addEventListener("click", function () {
+  localStorage.setItem("timezone", timezone.value);
+  localStorage.setItem("switch1", switch1.checked);
+  localStorage.setItem("switch2", switch2.checked);
+});
+
+cancel.addEventListener("click", function () {
+  localStorage.clear();
+  timezone.value = "";
+  switch1.checked = false;
+  switch2.checked = false;
+});
+
+window.onload = function () {
+  if (timezone.value) {
+    timezone.value = localStorage.getItem("timezone");
+  } else {
+    timezone.value = "";
+  }
+  switch1.checked = localStorage.getItem("switch1");
+  switch2.checked = localStorage.getItem("switch2");
+};
+
+/* Notifications pop-up */
+
+const notificationIcon = document.getElementById("notification-icon");
+
+notificationIcon.addEventListener("click", function () {});
+
+/* Message user widget*/
+
+const send = document.getElementsByClassName("submit-message")[0];
+const user = document.getElementById("input-search");
+const message = document.getElementById("input-message");
+
+send.addEventListener("click", function () {
+  if (user.value && message.value) {
+    alert("Your message was sent successfully!");
+    user.value = "";
+    message.value = "";
+  } else if (!user.value) {
+    alert("Please select a user");
+  } else if (!message.value) {
+    alert("Please enter a message");
+  }
+});
+
+/* Autocomplete search user*/
+var tags = ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"];
+$("#input-search").autocomplete({ source: tags });
